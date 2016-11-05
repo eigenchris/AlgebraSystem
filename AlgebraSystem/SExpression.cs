8dd3233cae81beb0af31aca6d7c051b04eb186da
Chris D <Chris D>
2016-11-04 22:45:05 -0700

    First commit of AlgebraSystem with SExpression and unit tests.

---
+++ AlgebraSystem/SExpression.cs
@@ -0,0 +1,110 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AlgebraSystem {
+    public class SExpression {
+        public string value;
+        public SExpression left;
+        public SExpression right;
+
+        public SExpression(string s) {
+            this.value = s;
+            this.left = null;
+            this.right = null;
+        }
+
+        public SExpression(SExpression left, SExpression right) {
+            this.value = null;
+            this.left = left;
+            this.right = right;
+        }
+
+        public override string ToString() {
+            if (this.value != null) return this.value;
+            if (this.left.value==null) {
+                return "(" + this.left + ") " + this.right;
+            }
+            return this.left + " " + this.right;
+        }
+
+        // methods in here return the number of parsed characters
+        public static int Identifier(string input, int start = 0) {
+            int length = 0;
+            while (start+length < input.Length && !"() ".Contains(input[start+length])) {
+                length++;
+            }         
+            return length;
+        }
+
+        public static int Spaces(string input, int start = 0) {
+            int length = 0;
+            while (length < input.Length && char.IsWhiteSpace(input[start+length])) {
+                length++;
+            }
+            return length;
+        }
+
+        public static bool CheckBalancedParens(string s) {
+            int depth = 0;
+            foreach (var c in s) {
+                if (c == '(') depth++;
+                if (c == ')') depth--;
+                if (depth < 0) return false;
+            }
+            return depth == 0;
+        }
+
+        public static int GetIndexOfEndParen(string s, int start = 0) {
+            int depth = 1;
+            int idx = start;
+            while (true) {
+                if (idx >= s.Length) return -1;
+                char c = s[idx];
+                if (c == '(') depth++;
+                if (c == ')') depth--;
+                if (depth == 0) break;
+                idx++;
+            }
+            return idx;
+        }
+
+        public static SExpression ParseSExpression(string s) {
+            bool balanced = CheckBalancedParens(s);
+            if (!balanced) throw new Exception("Parentheses not balanced!");
+            return ParseSExpressionRecur(s, 0, s.Length-1);
+        }
+
+        public static SExpression ParseSExpressionRecur(string s, int left, int right) {
+            if (left > right) throw new Exception("Parentheses not balanced!");
+            while (s[left] == ' ') left++;
+            while (s[right] == ' ') right--;
+            if (left > right) throw new Exception("Parentheses not balanced!");
+
+            SExpression sexp;
+            int afterEnd;
+            if (s[left] == '(') {
+                int end = GetIndexOfEndParen(s, left + 1);
+                sexp = ParseSExpressionRecur(s, left + 1, end - 1);
+                afterEnd = end + 1;
+            } else {
+                int length = Identifier(s, left);
+                string identifier = s.Substring(left, length);
+                sexp = new SExpression(identifier);
+                afterEnd = left + length;
+            }
+
+            if (afterEnd > right) {
+                return sexp;
+            }
+
+            int spaces = Spaces(s, afterEnd);
+            SExpression leftSubExp = sexp;
+            SExpression rightSubExp = ParseSExpressionRecur(s, afterEnd + spaces, right);
+            return new SExpression(leftSubExp, rightSubExp);
+        }
+
+    }
+}
---
+++ AlgebraSystemTest/SExpressionTest.cs
@@ -0,0 +1,73 @@
+﻿
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using AlgebraSystem;
+
+namespace UnitTestProject1 {
+
+    [TestClass]
+    public class SExpressionTest {
+
+        [TestMethod]
+        public void SExpressionTest_CheckBalancedParenTest() {
+            // Arrange
+            string success1 = "";
+            string success2 = "((((()))))";
+            string success3 = "()()()()()";
+            string success4 = "(())(()(()))";
+
+            string fail1 = ")";
+            string fail2 = "()()(((";
+            string fail3 = "()(()))";
+
+            // Act & Assert
+            Assert.IsTrue(SExpression.CheckBalancedParens(success1));
+            Assert.IsTrue(SExpression.CheckBalancedParens(success2));
+            Assert.IsTrue(SExpression.CheckBalancedParens(success3));
+            Assert.IsTrue(SExpression.CheckBalancedParens(success4));
+
+            Assert.IsFalse(SExpression.CheckBalancedParens(fail1));
+            Assert.IsFalse(SExpression.CheckBalancedParens(fail2));
+            Assert.IsFalse(SExpression.CheckBalancedParens(fail3));            
+        }
+
+        [TestMethod]
+        public void SExpressionTest_GetIndexOfEndParenTest() {
+            // Arrange
+            string success1 = "((w+rds)))";
+            int start1 = 2; int end1 = 7;
+            string success2 = "w*rds(w%rds)words)  ";
+            int start2 = 0; int end2 = 17;
+            string success3 = "()(()))";
+            int start3 = 0; int end3 = 6;
+
+            string fail1 = "";
+            string fail2 = "(";
+            string fail3 = "()(()";
+
+            // Act & Assert
+            Assert.AreEqual(end1,SExpression.GetIndexOfEndParen(success1,start1));
+            Assert.AreEqual(end2,SExpression.GetIndexOfEndParen(success2,start2));
+            Assert.AreEqual(end3,SExpression.GetIndexOfEndParen(success3,start3));
+
+            Assert.AreEqual(-1, SExpression.GetIndexOfEndParen(fail1));
+            Assert.AreEqual(-1, SExpression.GetIndexOfEndParen(fail2));
+            Assert.AreEqual(-1, SExpression.GetIndexOfEndParen(fail3));
+        }
+
+        [TestMethod]
+        public void SExpressionTest_ParseSExpressionSuccessTest() {
+            string success0 = "  x  ";
+            string success1 = "(  xyz  )";
+            string success2 = "f a b c";
+            string success3 = "((f) ((a) ((b) (c))))";
+            string success4 = " ( f  x )    ( g   y  )  z  ";
+
+            var sexp0 = SExpression.ParseSExpression(success0);
+            var sexp1 = SExpression.ParseSExpression(success1);
+            var sexp2 = SExpression.ParseSExpression(success2);
+            var sexp3 = SExpression.ParseSExpression(success3);
+            var sexp4 = SExpression.ParseSExpression(success4);
+        }
+
+    }
+}
